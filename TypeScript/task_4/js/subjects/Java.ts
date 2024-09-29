bf764a6dbddef4287916220c2e9739ae3b96a504
MuhammedSultanov <9073@holbertonstudents.com>
2024-09-29 18:17:10 +0400

    task4

---
+++ TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,18 @@
+namespace Subjects {
+    export interface Teacher {
+      experienceTeachingJava?: number;
+    }
+  
+    export class Java extends Subject {
+      getRequirements(): string {
+        return 'Here is the list of requirements for Java';
+      }
+  
+      getAvailableTeacher(): string {
+        return this.teacher.experienceTeachingJava
+          ? `Available Teacher: ${this.teacher.firstName}`
+          : 'No available teacher';
+      }
+    }
+  }
+  
